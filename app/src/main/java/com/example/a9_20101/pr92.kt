becb85be19678758c19162c5d728b4b598bb0665
PurpleAss <purtovajagg06@gmail.com>
2022-05-06 11:50:34 +0700

    2 commit

---
+++ app/src/main/java/com/example/a9_20101/pr92.kt
@@ -0,0 +1,11 @@
+package com.example.a9_20101
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class pr92 : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_pr92)
+    }
+}
